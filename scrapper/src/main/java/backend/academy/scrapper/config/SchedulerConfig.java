d96fcdc7b2fb72f9c099899ac6a569dca57f2d80
Delphington <benjaminvourts@gmail.com>
2025-02-07 16:13:51 +0300

    feat: separate SchedulerConfig

---
+++ scrapper/src/main/java/backend/academy/scrapper/config/SchedulerConfig.java
@@ -0,0 +1,12 @@
+package backend.academy.scrapper.config;
+
+import jakarta.validation.constraints.NotNull;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+import java.time.Duration;
+
+@Validated
+@ConfigurationProperties(prefix = "scheduler", ignoreUnknownFields = false)
+public record SchedulerConfig(boolean enable, @NotNull Duration interval, @NotNull Duration forceCheckDelay) {
+}
