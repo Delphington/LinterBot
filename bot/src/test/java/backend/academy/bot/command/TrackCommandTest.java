5b3b53864d26307d7ebc80d1b4fe15c269c4c061
Delphington <89201997+Delphington@users.noreply.github.com>
2025-02-26 00:53:00 +0300

    refactoring

--- bot/src/test/java/backend/academy/bot/command/TrackCommandTest.java
+++ bot/src/test/java/backend/academy/bot/command/TrackCommandTest.java
@@ -8,6 +8,7 @@ import static org.mockito.Mockito.when;
 
 import backend.academy.bot.api.ScrapperClient;
 import backend.academy.bot.api.dto.request.AddLinkRequest;
+import backend.academy.bot.api.dto.response.LinkResponse;
 import backend.academy.bot.api.exception.ResponseException;
 import backend.academy.bot.exception.InvalidInputFormatException;
 import backend.academy.bot.message.ParserMessage;
@@ -15,6 +16,7 @@ import backend.academy.bot.state.UserState;
 import backend.academy.bot.state.UserStateManager;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,6 +25,8 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import java.net.URI;
+import java.util.List;
 
 public class TrackCommandTest extends BaseCommandTest {
 
@@ -75,8 +79,8 @@ public class TrackCommandTest extends BaseCommandTest {
 
         // Assert
         assertEquals(
-                "Введите теги через пробел для ссылки",
-                sendMessage.getParameters().get("text"));
+            "Введите теги через пробел для ссылки",
+            sendMessage.getParameters().get("text"));
     }
 
     @Test
@@ -89,16 +93,16 @@ public class TrackCommandTest extends BaseCommandTest {
         when(userStateManager.getUserState(5L)).thenReturn(UserState.WAITING_URL);
 
         doThrow(new InvalidInputFormatException("Use a valid URL as a parameter in the form like '/track <link>'"))
-                .when(parserMessage)
-                .parseUrl(commandMessage, UserState.WAITING_URL);
+            .when(parserMessage)
+            .parseUrl(commandMessage, UserState.WAITING_URL);
 
         // Act
         SendMessage sendMessage = trackCommand.handle(update);
 
         // Assert
         assertEquals(
-                "Use a valid URL as a parameter in the form like '/track <link>'",
-                sendMessage.getParameters().get("text"));
+            "Use a valid URL as a parameter in the form like '/track <link>'",
+            sendMessage.getParameters().get("text"));
     }
 
     @Test
@@ -111,49 +115,16 @@ public class TrackCommandTest extends BaseCommandTest {
 
         when(userStateManager.getUserState(chatId)).thenReturn(UserState.WAITING_TAGS);
 
-        // Act
         SendMessage sendMessage = trackCommand.handle(update);
 
-        // Assert
         assertEquals(
-                "Введите фильтры через пробел для ссылки",
-                sendMessage.getParameters().get("text"));
+            "Введите фильтры через пробел для ссылки",
+            sendMessage.getParameters().get("text"));
     }
-
-    //    @Disabled
-    //    @Test
-    //    @DisplayName("Успешное добавление ссылки тегов и фильтров")
-    //    void handleFiltersInput() {
-    //        // Arrange
-    //        Long chatId = 5L;
-    //        String filtersMessage = "filter1 filter2";
-    //        Update update = getMockUpdate(chatId, filtersMessage);
-    //
-    //        when(userStateManager.getUserState(chatId)).thenReturn(UserState.WAITING_FILTERS);
-    //
-    //        LinkResponse linkResponse = new LinkResponse(
-    //                1L,
-    //                URI.create("https://github.com/"),
-    //                List.of("tag1", "tag2"),
-    //                List.of("filter1", "filter2")
-    //        );
-    //        when(scrapperClient.trackLink(eq(chatId), any(AddLinkRequest.class))).thenReturn(linkResponse);
-    //
-    //        // Act
-    //        SendMessage sendMessage = trackCommand.handle(update);
-    //
-    //        // Assert
-    //        String expectedMessage = "Ссылка добавлена!\n" +
-    //            "URL: https://github.com/\n" +
-    //            "tags: [tag1, tag2]\n" +
-    //            "filters: [filter1, filter2]";
-    //        Assertions.assertEquals(expectedMessage, sendMessage.getParameters().get("text"));
-    //    }
 
     @Test
     @DisplayName("Повторное добавление ссылки")
     void handleDuplicateLink() {
-        // Arrange
         Long chatId = 5L;
         String filtersMessage = "filter1 filter2";
         Update update = getMockUpdate(chatId, filtersMessage);
@@ -161,21 +132,44 @@ public class TrackCommandTest extends BaseCommandTest {
         when(userStateManager.getUserState(chatId)).thenReturn(UserState.WAITING_FILTERS);
 
         when(scrapperClient.trackLink(eq(chatId), any(AddLinkRequest.class)))
-                .thenThrow(new ResponseException("Link already exists"));
+            .thenThrow(new ResponseException("Link already exists"));
 
-        // Act
         SendMessage sendMessage = trackCommand.handle(update);
 
-        // Assert
         assertEquals(
-                "Такая ссылка уже добавлена, добавьте новую ссылку используя /track",
-                sendMessage.getParameters().get("text"));
+            "Такая ссылка уже добавлена, добавьте новую ссылку используя /track",
+            sendMessage.getParameters().get("text"));
     }
+
+//    @Test
+//    @DisplayName("Успешное добавление ссылки тегов и фильтров")
+//    void handleFiltersInput() {
+//        Long chatId = 5L;
+//        String filtersMessage = "filter1 filter2";
+//        Update update = getMockUpdate(chatId, filtersMessage);
+//
+//        when(userStateManager.getUserState(chatId)).thenReturn(UserState.WAITING_FILTERS);
+//
+//        LinkResponse linkResponse = new LinkResponse(
+//            1L,
+//            URI.create("https://github.com/"),
+//            List.of("tag1", "tag2"),
+//            List.of("filter1", "filter2")
+//        );
+//        when(scrapperClient.trackLink(eq(chatId), any(AddLinkRequest.class))).thenReturn(linkResponse);
+//
+//        SendMessage sendMessage = trackCommand.handle(update);
+//
+//        String expectedMessage = "Ссылка добавлена!\n" +
+//            "URL: https://github.com/\n" +
+//            "tags: [tag1, tag2]\n" +
+//            "filters: [filter1, filter2]";
+//        assertEquals(expectedMessage, sendMessage.getParameters().get("text"));
+//    }
 
     @Test
     @DisplayName("Проверка пустых тегов")
     void handleInvalidTagsInput() {
-        // Arrange
         Long chatId = 5L;
         String invalidTagsMessage = "";
         Update update = getMockUpdate(chatId, invalidTagsMessage);
@@ -183,36 +177,30 @@ public class TrackCommandTest extends BaseCommandTest {
         when(userStateManager.getUserState(chatId)).thenReturn(UserState.WAITING_TAGS);
 
         doThrow(new InvalidInputFormatException("Теги не могут быть пустыми"))
-                .when(parserMessage)
-                .getAdditionalAttribute(invalidTagsMessage);
+            .when(parserMessage)
+            .getAdditionalAttribute(invalidTagsMessage);
 
-        // Act
         SendMessage sendMessage = trackCommand.handle(update);
 
-        // Assert
         assertEquals("Теги не могут быть пустыми", sendMessage.getParameters().get("text"));
     }
 
     @Test
     @DisplayName("Проверка пустых фильтров")
     void handleInvalidFiltersInput() {
-        // Arrange
         Long chatId = 5L;
         String invalidFiltersMessage = "";
         Update update = getMockUpdate(chatId, invalidFiltersMessage);
 
-        // Устанавливаем состояние пользователя в WAITING_FILTERS
         when(userStateManager.getUserState(chatId)).thenReturn(UserState.WAITING_FILTERS);
 
         doThrow(new InvalidInputFormatException("Фильтры не могут быть пустыми"))
-                .when(parserMessage)
-                .getAdditionalAttribute(invalidFiltersMessage);
+            .when(parserMessage)
+            .getAdditionalAttribute(invalidFiltersMessage);
 
-        // Act
         SendMessage sendMessage = trackCommand.handle(update);
 
-        // Assert
         assertEquals(
-                "Фильтры не могут быть пустыми", sendMessage.getParameters().get("text"));
+            "Фильтры не могут быть пустыми", sendMessage.getParameters().get("text"));
     }
 }
