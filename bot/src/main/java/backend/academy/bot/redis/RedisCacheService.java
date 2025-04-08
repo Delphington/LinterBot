5b5fdf5fa4fd1988a3d5131ed595cea7a5ad96a1
Delphington <89201997+Delphington@users.noreply.github.com>
2025-04-08 23:32:22 +0300

    feat: redis cache

---
+++ bot/src/main/java/backend/academy/bot/redis/RedisCacheService.java
@@ -0,0 +1,35 @@
+package backend.academy.bot.redis;
+
+import backend.academy.bot.api.dto.response.ListLinksResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class RedisCacheService {
+
+    private static final String CACHE_PREFIX = "bot:links";
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public void cacheLinks(Long chatId, ListLinksResponse response) {
+        redisTemplate.opsForValue().set(
+            buildKey(chatId),
+            response
+        );
+    }
+
+    public ListLinksResponse getCachedLinks(Long chatId) {
+        return (ListLinksResponse) redisTemplate.opsForValue().get(buildKey(chatId));
+    }
+
+    public void invalidateCache(Long chatId) {
+        redisTemplate.delete(buildKey(chatId));
+    }
+
+    private String buildKey(Long chatId) {
+        return CACHE_PREFIX + ":" + chatId;
+    }
+}
