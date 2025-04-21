3cbe1388b0c4323dd5027e633b5985cf3acecd5e
Delphington <89201997+Delphington@users.noreply.github.com>
2025-04-21 18:11:50 +0300

    test: kafka

---
+++ bot/src/test/java/backend/academy/bot/integration/KafkaTestContainer.java
@@ -0,0 +1,54 @@
+package backend.academy.bot.integration;
+
+import backend.academy.bot.api.dto.kafka.BadLink;
+import backend.academy.bot.api.dto.request.LinkUpdate;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.containers.KafkaContainer;
+import java.util.HashMap;
+import java.util.Map;
+
+@TestConfiguration
+public class KafkaTestContainer {
+
+    @Container
+    public static KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.5.0"));
+
+    static {
+        kafka.start();
+    }
+
+    @DynamicPropertySource
+    static void kafkaProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
+    }
+
+     public static KafkaTemplate<String, LinkUpdate> createKafkaTemplate() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafka.getBootstrapServers());
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, org.apache.kafka.common.serialization.StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        ProducerFactory<String, LinkUpdate> producerFactory = new DefaultKafkaProducerFactory<>(configProps);
+        return new KafkaTemplate<>(producerFactory);
+    }
+
+
+    public static KafkaTemplate<String, BadLink> createKafkaTemplateBad() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafka.getBootstrapServers());
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        return new KafkaTemplate<>(new DefaultKafkaProducerFactory<>(config));
+    }
+}
--- bot/src/test/java/backend/academy/bot/integration/kafka/KafkaLinkUpdateListenerTest.java
+++ bot/src/test/java/backend/academy/bot/integration/kafka/KafkaLinkUpdateListenerTest.java
@@ -1,98 +1,81 @@
-// package backend.academy.bot.integration.kafka;
-//
-// import backend.academy.bot.api.dto.request.LinkUpdate;
-// import backend.academy.bot.kafka.client.KafkaLinkUpdateListener;
-// import backend.academy.bot.notification.MessageUpdateSender;
-// import backend.academy.bot.notification.NotificationProperties;
-// import backend.academy.bot.notification.NotificationService;
-// import backend.academy.bot.redis.RedisMessageService;
-// import org.junit.jupiter.api.BeforeAll;
-// import org.junit.jupiter.api.BeforeEach;
-// import org.junit.jupiter.api.DisplayName;
-// import org.junit.jupiter.api.Test;
-// import org.mockito.Mock;
-// import org.mockito.Mockito;
-// import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
-// import org.springframework.boot.test.context.SpringBootTest;
-// import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-// import org.springframework.kafka.core.KafkaTemplate;
-// import org.springframework.test.annotation.DirtiesContext;
-// import org.springframework.test.context.DynamicPropertyRegistry;
-// import org.springframework.test.context.DynamicPropertySource;
-// import org.springframework.test.util.ReflectionTestUtils;
-// import org.testcontainers.junit.jupiter.Container;
-// import org.testcontainers.junit.jupiter.Testcontainers;
-// import org.testcontainers.kafka.KafkaContainer;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.testcontainers.utility.DockerImageName;
-// import java.net.URI;
-// import java.time.Duration;
-// import java.util.List;
-// import java.util.concurrent.TimeUnit;
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-// import static org.junit.jupiter.api.Assertions.assertTrue;
-// import static org.mockito.Mockito.mock;
-// import static org.mockito.Mockito.timeout;
-// import static org.mockito.Mockito.verify;
-// import static org.springframework.test.annotation.DirtiesContext.ClassMode.AFTER_CLASS;
-// import static org.testcontainers.shaded.org.awaitility.Awaitility.await;
-// @SpringBootTest(classes = KafkaAutoConfiguration.class)
-// @Testcontainers
-// public class KafkaLinkUpdateListenerTest {
-//
-//    @Container
-//    public static final KafkaContainer KAFKA = new KafkaContainer(
-//        DockerImageName.parse("confluentinc/cp-kafka:7.3.0")
-//            .asCompatibleSubstituteFor("apache/kafka")
-//    )
-//        .withEnv("KAFKA_AUTO_CREATE_TOPICS_ENABLE", "true")
-//        .withStartupTimeout(Duration.ofMinutes(2));
-//
-//    @DynamicPropertySource
-//    static void kafkaProperties(DynamicPropertyRegistry registry) {
-//        registry.add("spring.kafka.bootstrap-servers", KAFKA::getBootstrapServers);
-//    }
-//
-//    @Test
-//    void shouldSendKafkaMessage() {
-//        assertTrue(KAFKA.isRunning());
-//    }
-// }
-////
-////    @DynamicPropertySource
-////    static void kafkaProperties(DynamicPropertyRegistry registry) {
-////        registry.add("spring.kafka.bootstrap-servers", KAFKA::getBootstrapServers);
-////        registry.add("spring.kafka.consumer.bootstrap-servers", KAFKA::getBootstrapServers);
-////        registry.add("spring.kafka.producer.bootstrap-servers", KAFKA::getBootstrapServers);
-////    }
-//
-////    @Autowired
-////    private KafkaTemplate<String, LinkUpdate> kafkaTemplate;
-////
-////    @Autowired
-////    private KafkaLinkUpdateListener kafkaListener;
-////
-////    private NotificationService notificationService;
-////
-////    @DynamicPropertySource
-////    static void kafkaProperties(DynamicPropertyRegistry registry) {
-////        registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
-////        registry.add("app.topic", () -> "updates");
-////    }
-////
-////    @BeforeEach
-////    void setUp() {
-////        notificationService = mock(NotificationService.class);
-////        ReflectionTestUtils.setField(kafkaListener, "notificationService", notificationService);
-////    }
-////
-////    @Test
-////    void shouldProcessKafkaMessage() throws Exception {
-////        LinkUpdate linkUpdate = new LinkUpdate(
-////            1L, URI.create("http://test.com"), "test", List.of(1L)
-////        );
-////
-////        kafkaTemplate.send("updates", linkUpdate).get(10, TimeUnit.SECONDS);
-////        verify(notificationService, timeout(5000)).sendMessage(linkUpdate);
-////    }
-//
+package backend.academy.bot.integration.kafka;
+
+import backend.academy.bot.api.dto.request.LinkUpdate;
+import backend.academy.bot.integration.KafkaTestContainer;
+import backend.academy.bot.kafka.client.KafkaLinkUpdateListener;
+import backend.academy.bot.notification.MessageUpdateSender;
+import backend.academy.bot.notification.NotificationProperties;
+import backend.academy.bot.notification.NotificationService;
+import backend.academy.bot.redis.RedisMessageService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.test.annotation.DirtiesContext;
+import java.net.URI;
+import java.util.List;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@Slf4j
+@DirtiesContext
+public class KafkaLinkUpdateListenerTest {
+
+
+    private KafkaTemplate<String, LinkUpdate> kafkaTemplate;
+
+    private KafkaLinkUpdateListener kafkaLinkUpdateListener;
+
+    @Mock
+    private NotificationProperties properties;
+
+    @Mock
+    private MessageUpdateSender messageUpdateSender;
+
+    @Mock
+    private RedisMessageService redisMessageService;
+
+    @Mock
+    private NotificationService notificationService;
+
+    private static final String TOPIC = "updated-topic";
+
+    @BeforeEach
+    void setUp() {
+        // Инициализация моков
+        MockitoAnnotations.openMocks(this);
+
+        // Настройка моков для messageUpdateSender
+        doNothing().when(messageUpdateSender).sendMessage(Mockito.any(LinkUpdate.class));
+        doNothing().when(notificationService).sendMessage(Mockito.any(LinkUpdate.class));
+
+        kafkaTemplate = KafkaTestContainer.createKafkaTemplate();
+        kafkaLinkUpdateListener = new KafkaLinkUpdateListener(notificationService);
+    }
+
+
+    @Test
+    @DisplayName("Тестирование KafkaUpdatesListener#listenUpdate с корректными данными")
+    public void listenUpdateShouldCatchUpdate() {
+        var linkUpdate = new LinkUpdate(
+            1L,
+            URI.create("http://test.com"),
+            "test",
+            List.of(1L)
+        );
+
+        // Отправляем сообщение в Kafka
+        kafkaTemplate.send(TOPIC, linkUpdate);
+
+        // Симулируем вызов метода updateConsumer, как если бы он был вызван KafkaListener
+        kafkaLinkUpdateListener.updateConsumer(linkUpdate, TOPIC);
+
+        // Проверяем, что метод sendMessage был вызван
+        verify(notificationService, times(1)).sendMessage(linkUpdate);
+    }
+}
