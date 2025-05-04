60df3c9fa0e122ebf9d323b4104cd16add187875
Delphington <89201997+Delphington@users.noreply.github.com>
2025-05-04 18:46:05 +0300

    fix: PR

--- scrapper/src/main/java/backend/academy/scrapper/tracker/client/StackOverFlowClient.java
+++ scrapper/src/main/java/backend/academy/scrapper/tracker/client/StackOverFlowClient.java
@@ -14,14 +14,14 @@ import lombok.extern.slf4j.Slf4j;
 public class StackOverFlowClient extends BaseWebClient {
 
     public StackOverFlowClient(
-            ScrapperConfig.StackOverflowCredentials stackOverflowCredentials, WebClientProperties webClientProperties) {
+        ScrapperConfig.StackOverflowCredentials stackOverflowCredentials, WebClientProperties webClientProperties) {
         super(stackOverflowCredentials.stackOverFlowUrl(), webClientProperties);
         if (stackOverflowCredentials.key() != null
-                && !stackOverflowCredentials.key().isEmpty()) {
+            && !stackOverflowCredentials.key().isEmpty()) {
             webClient.mutate().defaultHeader("key", stackOverflowCredentials.key());
         }
         if (stackOverflowCredentials.accessToken() != null
-                && !stackOverflowCredentials.accessToken().isEmpty()) {
+            && !stackOverflowCredentials.accessToken().isEmpty()) {
             webClient.mutate().defaultHeader("access_token", stackOverflowCredentials.accessToken());
         }
     }
@@ -29,67 +29,67 @@ public class StackOverFlowClient extends BaseWebClient {
     @Retry(name = "fetchQuestionStackOverFlow", fallbackMethod = "fetchQuestionFallback")
     public Optional<QuestionResponse> fetchQuestion(StackOverFlowRequest stackOverFlowRequest) {
         return Optional.ofNullable(webClient
-                .get()
-                .uri(uriBuilder -> uriBuilder
-                        .path("/questions/{chatId}")
-                        .queryParam("site", stackOverFlowRequest.site())
-                        .queryParam("order", stackOverFlowRequest.order())
-                        .queryParam("sort", stackOverFlowRequest.sort())
-                        .build(stackOverFlowRequest.number()))
-                .retrieve()
-                .bodyToMono(QuestionResponse.class)
-                .timeout(webClientProperties.globalTimeout())
-                .doOnError(error -> log.error("Ошибка при отправке запроса: {}", error.getMessage()))
-                .block());
+            .get()
+            .uri(uriBuilder -> uriBuilder
+                .path("/questions/{chatId}")
+                .queryParam("site", stackOverFlowRequest.site())
+                .queryParam("order", stackOverFlowRequest.order())
+                .queryParam("sort", stackOverFlowRequest.sort())
+                .build(stackOverFlowRequest.number()))
+            .retrieve()
+            .bodyToMono(QuestionResponse.class)
+            .timeout(webClientProperties.globalTimeout())
+            .doOnError(error -> log.error("Ошибка при отправке запроса:  {}", error.getMessage()))
+            .block());
     }
 
     @Retry(name = "fetchAnswerStackOverFlow", fallbackMethod = "fetchAnswerFallback")
     public Optional<AnswersResponse> fetchAnswer(StackOverFlowRequest stackOverFlowRequest) {
         return Optional.ofNullable(webClient
-                .get()
-                .uri(uriBuilder -> uriBuilder
-                        .path("/questions/{chatId}/answers")
-                        .queryParam("site", stackOverFlowRequest.site())
-                        .queryParam("filter", stackOverFlowRequest.filter())
-                        .build(stackOverFlowRequest.number()))
-                .retrieve()
-                .bodyToMono(AnswersResponse.class)
-                .timeout(webClientProperties.globalTimeout())
-                .doOnError(error -> log.error("Ошибка при отправке запроса: {}", error.getMessage()))
-                .block());
+            .get()
+            .uri(uriBuilder -> uriBuilder
+                .path("/questions/{chatId}/answers")
+                .queryParam("site", stackOverFlowRequest.site())
+                .queryParam("filter", stackOverFlowRequest.filter())
+                .build(stackOverFlowRequest.number()))
+            .retrieve()
+            .bodyToMono(AnswersResponse.class)
+            .timeout(webClientProperties.globalTimeout())
+            .doOnError(error -> log.error("Ошибка при отправке запроса: {}", error.getMessage()))
+            .block());
     }
 
     @Retry(name = "fetchCommentStackOverFlow", fallbackMethod = "fetchCommentFallback")
     public Optional<CommentResponse> fetchComment(StackOverFlowRequest stackOverFlowRequest) {
         return Optional.ofNullable(webClient
-                .get()
-                .uri(uriBuilder -> uriBuilder
-                        .path("/questions/{chatId}/comments")
-                        .queryParam("site", stackOverFlowRequest.site())
-                        .queryParam("filter", stackOverFlowRequest.filter())
-                        .build(stackOverFlowRequest.number()))
-                .retrieve()
-                .bodyToMono(CommentResponse.class)
-                .timeout(webClientProperties.globalTimeout())
-                .doOnError(error -> log.error("Ошибка при отправке запроса: {}", error.getMessage()))
-                .block());
+            .get()
+            .uri(uriBuilder -> uriBuilder
+                .path("/questions/{chatId}/comments")
+                .queryParam("site", stackOverFlowRequest.site())
+                .queryParam("filter", stackOverFlowRequest.filter())
+                .build(stackOverFlowRequest.number()))
+            .retrieve()
+            .bodyToMono(CommentResponse.class)
+            .timeout(webClientProperties.globalTimeout())
+            .doOnError(error -> log.error("Ошибка при отправке запроса : {}", error.getMessage()))
+            .block());
     }
 
     @SuppressWarnings({"UnusedMethod", "UnusedVariable"})
     private Optional<QuestionResponse> fetchQuestionFallback(StackOverFlowRequest stackOverFlowRequest, Exception ex) {
-        log.error("Произошла ошибка fetchQuestionFall: {}", ex.getMessage());
+        log.error("Произошла ошибка stackOverFlowRequest = {}, ex = {}", stackOverFlowRequest, ex.getMessage());
         return Optional.empty();
     }
 
     @SuppressWarnings({"UnusedMethod", "UnusedVariable"})
     private Optional<AnswersResponse> fetchAnswerFallback(StackOverFlowRequest stackOverFlowRequest, Exception ex) {
-        log.error("Произошла ошибка fetchAnswer: {}", ex.getMessage());
+        log.error("Произошла ошибка stackOverFlowRequest= {}, ex = {}", stackOverFlowRequest, ex.getMessage());
         return Optional.empty();
     }
 
-    @SuppressWarnings({"UnusedMethod", "UnusedVariable"})
+    @SuppressWarnings("PMD.UnusedPrivateMethod")
     private Optional<CommentResponse> fetchCommentFallback(StackOverFlowRequest stackOverFlowRequest, Exception ex) {
-        log.error("Произошла ошибка fetchComment: {}", ex.getMessage());
+        log.error("Произошла ошибка stackOverFlowRequest = {},ex = {}", stackOverFlowRequest, ex.getMessage());
         return Optional.empty();
     }
 }
